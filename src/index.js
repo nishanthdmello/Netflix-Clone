97847e56891f00b3a8a8f858c4f6d156664db858
Nishanth <106912949+nishanthdmello@users.noreply.github.com>
2022-10-11 22:25:43 +0530

    small changes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import ReactDOM from 'react-dom/client';
-import {NavBar, WelcomeSlide, Slide, Divider, Slide1, FAQ} from './Welcome';
+import ReactDOM from "react-dom/client";
+import { NavBar, WelcomeSlide, Slide, Divider, Slide1, FAQ } from "./Welcome";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <div style={{backgroundColor:"#3f053a"}}>
-    <NavBar/>
-    <WelcomeSlide/>
-    <Divider/>
-    <Slide/>
-    <Divider/>
-    <Slide1/>
-    <Divider/>
-    <FAQ/>
+  <div style={{ backgroundColor: "#3f053a" }}>
+    <NavBar />
+    <WelcomeSlide />
+    <Divider />
+    <Slide />
+    <Divider />
+    <Slide1 />
+    <Divider />
+    <FAQ />
   </div>
 );
