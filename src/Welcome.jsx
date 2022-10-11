c626c882dee07ed1d6d98208ae400d3fab270909
Nihal T M <nihaltmdev@gmail.com>
2022-10-11 14:19:54 +0530

    some small changes

--- src/Welcome.jsx
+++ src/Welcome.jsx
@@ -1,14 +1,15 @@
 import React from "react";
-import netflixlogo from "./resources/Netflix-Brand-Logo.png";
+import mflixlogo from "./resources/Mflix_Brand_Logo1.png";
 import sl1_bg from "./resources/netflix_sl1_bg.jpg";
 import netflix_on_tv from "./resources/netflix_on_tv.gif";
+import netflix_children_profiles from "./resources/netflix_children_profiles.png";
 
 class NavBar extends React.Component {
     render() {
         return(
-            <nav style={{backgroundColor:"black", color:"white", display:"inline-block"}}>
-                <img src={netflixlogo} alt="demo pic" style={{width:"150px", height:"80px"}}/>
-                <button style={{backgroundColor:"red", color:"white", display:"inline-block", width:"100px", height:"40px",borderRadius:"5px",position:"absolute", margin:"20px", right:"20px"}}><b>Sign In</b></button>    
+            <nav style={{backgroundColor:"#3f053a", color:"white", display:"inline-block"}}>
+                <img src={mflixlogo} alt="demo pic" style={{width:"150px", height:"80px"}}/>
+                <button style={{backgroundColor:"#db4dce", color:"white", display:"inline-block", width:"100px", height:"40px",borderRadius:"5px",position:"absolute", margin:"20px", right:"20px"}}><b>Sign In</b></button>    
             </nav>
         )
     }
@@ -19,13 +20,13 @@ class WelcomeSlide extends React.Component {
         return(
             <div>
                 <img src={sl1_bg} alt="demo pic" style={{zIndex:"-5", width:"1335px", height:"575px", opacity:"50%"}} />
-                <center style={{zIndex:"0", position:"absolute", top:"300px", left:"20%"}}>
+                <center style={{zIndex:"0", position:"absolute", top:"250px", left:"15%"}}>
                     <p style={{fontSize:"30px", color:"white", fontFamily: "Netflix Sans,Helvetica Neue,Helvetica,Arial,sans-serif"}}>
                         Subscribe to get unlimited access to Movies and TV Shows that you can <br/>
                         watch from anywhere anytime. Cancel Subscription when you want to.
                     </p>
-                    <input type="email" placeholder="Enter your email address" style={{width:"400px", height:"50px", borderRadius:"10px"}}/>
-
+                    <input type="email" placeholder="Enter your email address" style={{width:"400px", height:"40px", borderRadius:"10px"}}/>
+                    <button style={{backgroundColor:"#db4dce", color:"white", display:"inline-block", width:"100px", height:"40px",borderRadius:"5px"}}><b>See Plans</b></button>
                 </center>
             </div>
         )
@@ -35,11 +36,61 @@ class WelcomeSlide extends React.Component {
 class Slide extends React.Component {
     render() {
         return(
-            <div style={{backgroundColor:"black", display:"inline-block"}}>
+            <div style={{backgroundColor:"black", display:"inline-block", height:"350px"}}>
                 <img src={netflix_on_tv} alt="demo pic"/>
                 <p style={{color:"white", position:"absolute", padding:"100px", display:"inline-block", fontSize:"30px", fontFamily: "Netflix Sans,Helvetica Neue,Helvetica,Arial,sans-serif"}}>
                     Watch your favourite Movies and TV Shows on your big screen
                 </p>
+            </div>
+        )
+    }
+}
+
+class Slide1 extends React.Component {
+    render() {
+        return(
+            <div style={{backgroundColor:"black", display:"inline-block", height:"350px"}}>
+                <img src={netflix_children_profiles} alt="demo pic"/>
+                <p style={{color:"white", position:"absolute", padding:"100px", display:"inline-block", fontSize:"30px", fontFamily: "Netflix Sans,Helvetica Neue,Helvetica,Arial,sans-serif"}}>
+                    Create Profiles for Kids and set<br/> them free to play and explore in<br/> entertainment made for them
+                </p>
+            </div>
+        )
+    }
+}
+
+class FAQButton extends React.Component {
+    render () {
+        const faqb={
+            
+        }
+        return (
+            <div>
+                <button style={faqb}>{this.props.children}</button>
+            </div>
+        )
+    }
+}
+
+class FAQ extends React.Component {
+    render() {
+        return(
+            <div style={{backgroundColor:"black"}}>
+                <button>
+                    1. What is MFLIX?
+                </button>
+                <button>
+                    2. What plans can I choose?
+                </button>
+                <button>
+                    3. How many devices can I use this on?
+                </button>
+                <button>
+                    4. When can I cancel my Subscription
+                </button>
+                <button>
+                    5. Are there any hidden charges for deleting my subscription?
+                </button>
             </div>
         )
     }
@@ -57,5 +108,7 @@ export {
     NavBar,
     Divider,
     WelcomeSlide,
-    Slide
+    Slide,
+    Slide1,
+    FAQ
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import ReactDOM from 'react-dom/client';
-import {NavBar, WelcomeSlide, Slide, Divider} from './Welcome';
+import {NavBar, WelcomeSlide, Slide, Divider, Slide1, FAQ} from './Welcome';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <div style={{backgroundColor:"black"}}>
+  <div style={{backgroundColor:"#3f053a"}}>
     <NavBar/>
     <WelcomeSlide/>
     <Divider/>
     <Slide/>
     <Divider/>
+    <Slide1/>
+    <Divider/>
+    <FAQ/>
   </div>
 );
